cd500fbee32236c9113722bd73d03ec9adaeb97a
Mdsr2op <mdsrmfkhan@gmail.com>
2024-12-12 16:14:47 +0500

    Added boilerplate code for react typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <div className="text-3xl font-bold underline">Hello world</div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
