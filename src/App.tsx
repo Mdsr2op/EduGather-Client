bae19def7524fb769398279e89d10c1a4ecff5de
Mdsr2op <mdsrmfkhan@gmail.com>
2024-12-12 21:44:14 +0500

    Added AuthLayout along with routes for signup and signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { Button } from "./components/ui/button";
+import { Routes, Route } from "react-router-dom";
 import { SignInForm } from "./features/auth/components/SignInForm";
 import { SignUpForm } from "./features/auth/components/SignUpForm";
+import AuthLayout from "./features/auth/layout/AuthLayout";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <div>
-      <SignInForm />
-    </div>
+    <Routes>
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route path="/sign-in" element={<SignInForm />} />
+      </Route>
+      <Route index element={<Button>Click me</Button>}/>
+    </Routes>
   );
 }
 
