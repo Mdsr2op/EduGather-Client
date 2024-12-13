ea8d4068f390fab55ae36ca76c0dafdfe93645ec
Abdul Mateen <148681799+MateenKhan12@users.noreply.github.com>
2024-12-13 23:26:29 +0500

    added feature section to the home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Features } from "@/features/Home/components/Features";
 import { Header } from "@/features/Home/components/Header";
 import { Hero } from "@/features/Home/components/Hero";
 
@@ -6,6 +7,7 @@ const Home = () => {
     <div className="">
       <Header />
       <Hero />
+      <Features />
     </div>
   );
 };
