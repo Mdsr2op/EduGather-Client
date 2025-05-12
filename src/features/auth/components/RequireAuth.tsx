25dc7ff519d796e2a425bad94d0f8020cf506cff
Mdsr2op <mdsrmfkhan@gmail.com>
2025-05-12 21:45:56 +0500

    Updated RequireAuth component to redirect unauthenticated users to the landing page instead of the sign-in page.

--- src/features/auth/components/RequireAuth.tsx
+++ src/features/auth/components/RequireAuth.tsx
@@ -8,7 +8,7 @@ const RequireAuth: React.FC = () => {
   const location = useLocation();
 
   if (!user) {
-    return <Navigate to="/sign-in" state={{ from: location }} replace />;
+    return <Navigate to="/landing" state={{ from: location }} replace />;
   }
 
   return <Outlet />;
