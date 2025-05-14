3dea2e462307cd822eaf7ed2305ba0346eb14c6c
Mdsr2op <mdsrmfkhan@gmail.com>
2025-05-14 11:41:30 +0500

    Fixed typescript errors

--- src/features/auth/landing/components/Footer.tsx
+++ src/features/auth/landing/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Facebook, Twitter, Instagram, Linkedin } from 'lucide-react';
+import {  Twitter, Instagram, Linkedin } from 'lucide-react';
 
 export function Footer() {
   return (
