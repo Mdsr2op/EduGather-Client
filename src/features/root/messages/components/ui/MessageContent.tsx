0dbae57b1dc5885c5306fe837aff0fd37952ff27
Mdsr2op <mdsrmfkhan@gmail.com>
2025-05-21 11:59:52 +0500

    Updated MessageContent component to adjust text size classes for improved responsiveness across different screen sizes.

--- src/features/root/messages/components/ui/MessageContent.tsx
+++ src/features/root/messages/components/ui/MessageContent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { MessageContentProps } from '../../types/messageTypes';
 
 const MessageContent: React.FC<MessageContentProps> = ({ text }) => {
-  return <p className="text-lg mb-1 xs:text-sm">{text}</p>;
+  return <p className="text-base sm:text-base md:text-lg mb-1 xs:text-xs">{text}</p>;
 };
 
 export default MessageContent;
